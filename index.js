53976b15ef78662da994e7feb63b825c152a7f91
mdmhrz <razufreelance@gmail.com>
2025-06-19 03:32:58 +0600

    CRUD done

--- index.js
+++ index.js
@@ -39,13 +39,44 @@ async function run() {
             res.send(resutl)
         })
 
+        // Find one
+
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await usersCollection.findOne(query);
+            res.send(result)
+        })
+
         // Create
         app.post('/users', async (req, res) => {
             console.log('Data in the server', req.body);
             const newUser = req.body;
             const resutl = await usersCollection.insertOne(newUser);
             res.send(resutl);
+        });
+
+
+        // Update
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const user = req.body;
+
+            const updatedDoc = {
+                $set: {
+                    name: user.name,
+                    email: user.email
+                }
+            }
+
+            const options = { upsert: true };
+
+            console.log(user);
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
         })
+
 
         // Deleta
         app.delete('/users/:id', async (req, res) => {
